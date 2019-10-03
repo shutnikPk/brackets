95b14ac4fe2d1763e8aa6e9c847aad083d8c0e00
shutnikPk <51722234+shutnikPk@users.noreply.github.com>
2019-10-03 23:34:47 +0300

    brackets
    add: new fituers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = str.split('');
+    let tempStr = '';
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        for (let k = 0; k < bracketsConfig[i].length; k++) {
+            tempStr += bracketsConfig[i][k]
+        }
+    }
+
+    function stepIt() {
+        for (let k = 0; k <= tempStr.length; k += 2) {
+            for (let i = 0; i < arr.length; i++) {
+                if ((arr[i] == tempStr[k] && arr[i + 1] == tempStr[k + 1])) {
+                    delete arr[i];
+                    delete arr[i + 1];
+                    arr = arr.filter(function (el) {
+                        return el != undefined;
+                    });
+
+                }
+            }
+        }
+    }
+
+    for (let i = 0; i <= str.length / 2; i++) {
+        stepIt();
+    }
+    return arr.length === 0;
 }
